9d834dd2ed3e62ac840b679b20ffde3d7ea3402f
aklimaakter20 <aklimaakter20@gmail.com>
2025-01-29 20:39:43 +0600

    npm installed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,15 @@ function Hero() {
     <section>
       <div className="">
         <div className="flex flex-col-reverse md:flex-row items-center justify-center">
-          <div className="w-full md:w-[50%] px-12 pt-20">
-            <h2 className="text-xl py-[40px] md:text-2xl lg:text-5xl font-bold md:w-[70%] leading-tight">
+          <div className="w-full md:w-[50%] px-12 mt-10">
+            <h2 className="text-xl md:text-2xl lg:text-5xl font-bold md:w-[70%] leading-tight">
               Simply explained with illustrations
             </h2>
-            <p className="text-lg w-[70%] p-[40px]">
+            <p className="text-lg w-[70%] mt-3">
               There are a lot of different components that will help you create
               the perfect look for your project
             </p>
-            <div className="flex gap-10 mt-20">
+            <div className="flex gap-10 mt-5">
               <Button variant="contained">Get Started</Button>
               <Button variant="outlined">Learn More</Button>
             </div>
