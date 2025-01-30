46b4ac0b97c6de583404e808a193123e1375b264
aklimaakter20 <aklimaakter20@gmail.com>
2025-01-30 08:39:05 +0600

    Hero section responsive completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,16 +6,16 @@ function Hero() {
       <div className="">
         <div className="flex flex-col-reverse md:flex-row items-center justify-center">
           <div className="w-full md:w-[50%] px-12 mt-10">
-            <h2 className="text-xl md:text-2xl lg:text-5xl font-bold md:w-[70%] leading-tight">
+            <h2 className="text-xl md:text-2xl lg:text-5xl font-bold md:w-[90%] w-full md:text-start text-center leading-tight">
               Simply explained with illustrations
             </h2>
-            <p className="text-lg w-[70%] mt-3">
+            <p className="text-lg md:w-[90%] w-full text-center md:text-start mt-3">
               There are a lot of different components that will help you create
               the perfect look for your project
             </p>
-            <div className="flex gap-10 mt-5">
-              <Button variant="contained">Get Started</Button>
-              <Button variant="outlined">Learn More</Button>
+            <div className="flex md:gap-10 gap-3 mx-auto w-[150px] sm:w-[400px] md:ms-0 mt-5 flex-col sm:flex-row">
+              <Button variant="contained" className="w-[150px] text-sm md:text-xl">Get Started</Button>
+              <Button variant="outlined" className="w-[150px] text-sm md:text-xl">Learn More</Button>
             </div>
           </div>
           <div>
